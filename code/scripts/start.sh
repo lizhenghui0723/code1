0b961c806b753c39dcd1a2b9c0e56dd14710c75a
lizhenghui0723 <45784276+lizhenghui0723@users.noreply.github.com>
2025-06-18 01:33:53 +0800

    Delete code directory

--- code/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ../backend && uvicorn main:app --reload --host 0.0.0.0 --port 8000 &
-BACKEND_PID=$!
-cd ../frontend && npm start &
-FRONTEND_PID=$!
-echo $BACKEND_PID > ../scripts/backend.pid
-echo $FRONTEND_PID > ../scripts/frontend.pid
-echo "后端和前端已启动。" 
